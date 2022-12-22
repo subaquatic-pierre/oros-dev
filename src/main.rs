37bc8c3d6bb1d2ed36966d643cef89921a377f8d
Pierre du Toit <subaquatic.pierre@gmail.com>
2022-12-23 00:10:08 +0400

    builds and runs in qemu

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO_WORLD: &[u8] = b"Hello, World!";
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO_WORLD.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
     loop {}

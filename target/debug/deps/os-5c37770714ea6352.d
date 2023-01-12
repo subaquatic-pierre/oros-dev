207ee8c13fdf5ec441c045de71c987a431685b5c
Pierre du Toit <subaquatic.pierre@gmail.com>
2023-01-12 16:38:33 +0400

    Removed target

--- target/debug/deps/os-5c37770714ea6352.d
+++
@@ -1,7 +0,0 @@
-/home/pierre/Projects/Sandbox/oros/target/debug/deps/os-5c37770714ea6352.rmeta: src/main.rs
-
-/home/pierre/Projects/Sandbox/oros/target/debug/deps/os-5c37770714ea6352.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
